f84e6f03318349942b303792eb82366e3f0f88a1
tsfento <tsfento@gmail.com>
2024-01-18 19:46:15 +0000

    Finished Exercise 4

--- main.rb
+++ main.rb
@@ -22,3 +22,18 @@ end
 
 # Exercise 3
 "Hello world".each_char {|char| puts char}
+
+# Exercise 4
+i = 1
+while i <= 100
+  if i % 3 == 0 && i % 5 == 0
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
+    puts 'Buzz'
+  else
+    puts i
+  end
+  i += 1
+end
